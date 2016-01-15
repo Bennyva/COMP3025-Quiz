e47411bdd1246c1ab8602f6514f03182a743321b
student <student@students-mac.student.georgianc.on.ca>
2016-01-15 11:03:57 -0500

    Added Print function

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        //initial commit comment
+        print("View Loaded Success")
         
     }
 
